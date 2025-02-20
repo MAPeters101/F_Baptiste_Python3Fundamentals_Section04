4e6e363f11a2781711960056dc02c6ca5b63d4d8
Mark Peters <mapeters101@earthlink.net>
2025-02-20 16:14:32 -0500

    Work on lesson038 to 2:30.

---
+++ lesson038.py
@@ -0,0 +1,23 @@
+
+if 1 < 2:
+    print('1 is less than 2')
+
+if 1 > 2:
+    print('1 is greater than 2')
+
+if 1 < 2:
+    print('block - line 1')
+    print('block - line 2')
+    print('block - line 3')
+print('next line')
+print()
+
+if 1 > 2:
+    print('block - line 1')
+    print('block - line 2')
+    print('block - line 3')
+print('next line')
+print()
+
+
+
