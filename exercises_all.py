ee1b4f339aac5df64147e69fb8e622c61a98a3ad
Mark Peters <mapeters101@earthlink.net>
2025-02-20 18:20:05 -0500

    Description for all exercises reformated.

--- exercises_all.py
+++ exercises_all.py
@@ -1,12 +1,14 @@
 """
 Exercise 1
-Given a variable a (containing any value), re-assign the value "N/A" if a is None, and leave a unchanged otherwise. Use an if...else... statement.
+Given a variable a (containing any value), re-assign the value "N/A" if a is
+None, and leave a unchanged otherwise. Use an if...else... statement.
 
 Exercise 2
 Do the same thing as Question 1, but this time use a ternary operator.
 
 Exercise 3
-Given an credit score score, assign a string value to another variable rating based on the following scale:
+Given an credit score score, assign a string value to another variable rating
+based on the following scale:
 
 [0, 580) --> Poor
 [580, 670) --> Fair
@@ -14,7 +16,8 @@ Given an credit score score, assign a string value to another variable rating ba
 [740, 800) --> Very Good
 [800, 850] --> Excellent
 Exercise 4
-Given an elapsed time (in seconds), write code to set a variable magnitude based on the following conditions:
+Given an elapsed time (in seconds), write code to set a variable magnitude
+based on the following conditions:
 
 if elapsed time is less than 1 minute, magnitude --> 'seconds'
 if elapsed time is more than 1 minute, but less than 1 hour, magnitude --> 'minutes'
