f0c65052e0a8b981c71f483933110eac0d4a7b37
Mark Peters <mapeters101@earthlink.net>
2025-02-20 17:35:41 -0500

    Work on lesson040 from 1:30 to 3:00.

--- lesson040.py
+++ lesson040.py
@@ -8,6 +8,16 @@ else:
     else:
         print('Failed')
 
+print('-'*80)
+
+
+grade = 72
+if grade >= 90:
+    print('Passed with distinction')
+elif grade >= 70:
+    print('Passed')
+else:
+    print('Failed')
 
 
 
