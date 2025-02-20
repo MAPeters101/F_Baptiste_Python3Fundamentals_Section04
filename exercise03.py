1db59ea2eaa90a1ad61604d3f8047cd563caa2bd
Mark Peters <mapeters101@earthlink.net>
2025-02-20 18:30:49 -0500

    Description for exercise03.

---
+++ exercise03.py
@@ -0,0 +1,11 @@
+"""
+Exercise 3
+Given an credit score score, assign a string value to another variable rating
+based on the following scale:
+
+[0, 580) --> Poor
+[580, 670) --> Fair
+[670, 740) --> Good
+[740, 800) --> Very Good
+[800, 850] --> Excellent
+"""
