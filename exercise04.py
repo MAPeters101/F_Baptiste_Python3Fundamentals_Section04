c74e11684264d3acbc2d0736155ea9bf38ebc86f
Mark Peters <mapeters101@earthlink.net>
2025-02-20 18:32:26 -0500

    Description for exercise04.

---
+++ exercise04.py
@@ -0,0 +1,11 @@
+"""
+Exercise 4
+Given an elapsed time (in seconds), write code to set a variable magnitude
+based on the following conditions:
+
+if elapsed time is less than 1 minute, magnitude --> 'seconds'
+if elapsed time is more than 1 minute, but less than 1 hour, magnitude --> 'minutes'
+if elapsed time is more than 1 hour, but less than 1 day, magnitude --> 'hours'
+if elapsed time is more than 1 day, but less than 1 week: magnitude --> 'days'
+if elapsed time is more than 1 week, magnitude --> 'weeks'
+"""
