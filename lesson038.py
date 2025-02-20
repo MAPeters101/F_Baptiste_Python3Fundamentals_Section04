6ef7c808378c7422d84952b45d015a4f21fe5c0d
Mark Peters <mapeters101@earthlink.net>
2025-02-20 16:34:10 -0500

    Complete work on lesson038 from 8:00.

--- lesson038.py
+++ lesson038.py
@@ -82,6 +82,63 @@ else:
     else:
         print('insufficient funds')
 
+print('-'*80)
+
+grade = 72
+if grade >= 90:
+    letter_grade = 'A'
+else:
+    if grade >= 80:
+        letter_grade = 'B'
+    else:
+        if grade >= 70:
+            letter_grade = 'C'
+        else:
+            if grade >= 60:
+                letter_grade = 'D'
+            else:
+                letter_grade = 'F'
+
+print(letter_grade)
+print('-'*80)
+
+
+grade = 72
+if grade >= 90:
+    letter_grade = 'A'
+
+if grade >= 80 and grade < 90:
+    letter_grade = 'B'
+
+if grade >= 70 and grade < 80:
+    letter_grade = 'C'
+
+if grade >= 60 and grade < 70:
+    letter_grade = 'D'
+
+if grade < 60:
+    letter_grade = 'F'
+
+print(letter_grade)
+print('-'*80)
+
+
+grade = 72
+letter_grade = 'F'
+if grade >= 60:
+    letter_grade = 'D'
+
+if grade >= 70:
+    letter_grade = 'C'
+
+if grade >= 80:
+    letter_grade = 'B'
+
+if grade >= 90:
+    letter_grade = 'A'
+
+print(letter_grade)
+print('-'*80)
 
 
 
