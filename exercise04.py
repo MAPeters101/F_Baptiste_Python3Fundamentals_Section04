78df47b6e8bb473cf8e0f693b4f1efe1c0229935
Mark Peters <mapeters101@earthlink.net>
2025-02-21 16:01:54 -0500

    Complete exercise04.

--- exercise04.py
+++ exercise04.py
@@ -9,3 +9,26 @@ if elapsed time is more than 1 hour, but less than 1 day, magnitude --> 'hours'
 if elapsed time is more than 1 day, but less than 1 week: magnitude --> 'days'
 if elapsed time is more than 1 week, magnitude --> 'weeks'
 """
+
+elapsed_time = 3601
+MINUTE = 60
+HOUR = MINUTE * 60
+DAY = HOUR * 24
+WEEK = DAY * 7
+
+if elapsed_time > WEEK:
+    magnitude = 'weeks'
+elif elapsed_time > DAY:
+    magnitude = 'days'
+elif elapsed_time > HOUR:
+    magnitude = 'hours'
+elif elapsed_time > MINUTE:
+    magnitude = 'minutes'
+else:
+    magnitude = 'seconds'
+
+print(f'{elapsed_time} seconds will be {magnitude}.')
+
+
+
+
