e4fa8d8c7ae8cfab9e7ec0eb9e52208d91ae142f
Mark Peters <mapeters101@earthlink.net>
2025-02-21 14:45:27 -0500

    Work on lesson042 from 4:00 to 4:40.

--- lesson042.py
+++ lesson042.py
@@ -11,7 +11,27 @@ print()
 ask_price = 100
 volume = 50 if ask_price > 50 else 80
 print(volume)
+print('-'*80)
+
+
+a = 10
+b = 20
+print(abs(10 - 20))
 print()
+distance = a-b if a >= b else b-a
+print(distance)
+print()
+
+a = 20
+b = 10
+print(abs(10 - 20))
+print()
+distance = a-b if a >= b else b-a
+print(distance)
+print()
+
+
+
 
 
 
