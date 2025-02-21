5b2879b593bead764ec7e199ebf913cf07345ea5
Mark Peters <mapeters101@earthlink.net>
2025-02-21 15:36:18 -0500

    Complete exercise01.

--- exercise01.py
+++ exercise01.py
@@ -3,3 +3,10 @@ Exercise 1
 Given a variable a (containing any value), re-assign the value "N/A" if a is
 None, and leave a unchanged otherwise. Use an if...else... statement.
 """
+a = None
+
+if a is None:
+    a = 'N/A'
+
+print(a)
+
